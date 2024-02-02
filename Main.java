40f0f8db8b9c859bac9115718296bbd1ac48358d
Malte <maltebjoerklund@gmail.com>
2024-02-02 09:25:18 +0100

    Main.java

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        DynamicArray dArr = new DynamicArray();
+
+        dArr.add(3);
+        dArr.add(6);
+        dArr.add(14);
+        dArr.add(12);
+        dArr.add(17);
+        dArr.add(49);
+        dArr.add(23);
+
+        // System.out.printf("Element from index: %d%n", dArr.get(1));
+        // System.out.printf("Element from index: %d%n", dArr.get(0));
+        // System.out.printf("Element from index: %d%n", dArr.get(2));
+
+        // System.out.println(dArr.toString());
+        // dArr.remove();
+        // dArr.remove();
+        // dArr.remove();
+        // dArr.remove();
+
+        // System.out.println(dArr.toString());
+        // dArr.remove(3);
+        // dArr.remove(5);
+        // dArr.remove(4);
+        // dArr.remove(4);
+
+        // System.out.println(dArr.toString());
+        // dArr.set(1, 7);
+        // dArr.set(4, 15);
+        // dArr.set(3, 74);
+        // dArr.set(2, 88);
+
+        // System.out.println(dArr.toString());
+        // dArr.clear();
+    }
+}
